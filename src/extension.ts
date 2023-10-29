d9f559f9a5d59405eb07cb42f94c022e3511dbaf
alfredbirk <alfredbirk@gmail.com>
2023-10-30 00:03:47 +0100

    Cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,24 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "go-to-next-change" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "go-to-next-change" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand("go-to-next-change.go-to-next-scm-change", async () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        await goToNextDiff();
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('go-to-next-change.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from go-to-next-change!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 const isInDiffEditor = () => {
@@ -35,6 +34,7 @@ const isInDiffEditor = () => {
         )
     );
 };
+
 const openFirstFile = async () => {
     await vscode.commands.executeCommand("workbench.view.explorer");
     await vscode.commands.executeCommand("workbench.view.scm");
@@ -44,12 +44,14 @@ const openFirstFile = async () => {
     await vscode.commands.executeCommand("list.focusDown");
     await vscode.commands.executeCommand("list.select");
 };
+
 const openNextFile = async () => {
     await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
     await vscode.commands.executeCommand("workbench.view.scm");
     await vscode.commands.executeCommand("list.focusDown");
     await vscode.commands.executeCommand("list.select");
 };
+
 const goToNextDiff = async () => {
     const isDiffEditor = isInDiffEditor();
 
