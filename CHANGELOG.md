ddf67f7cef4830a010b92e38797fe8d324bcc7d3
alfredbirk <alfredbirk@gmail.com>
2023-10-31 14:02:00 +0100

    Close file after last change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,9 @@
 # Change Log
 
-All notable changes to the "go-to-next-change" extension will be documented in this file.
-
-Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## [Unreleased]
+## [1.0]
 
 - Initial release
+
+## [1.4.5]
+
+- Close file after last change
--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,26 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable, disposable2);
 }
 
+const getFileChanges = async () => {
+    const gitExtension = vscode.extensions.getExtension<any>("vscode.git")!.exports;
+    const git = gitExtension.getAPI(1);
+    const repos = git.repositories;
+    const changes = await repos[0].diffWithHEAD();
+    return changes;
+};
+
+const getFirstFilename = async () => {
+    const changes = await getFileChanges();
+    const filename = changes[0]?.renameUri.path;
+    return filename;
+};
+
+const getLastFilename = async () => {
+    const changes = await getFileChanges();
+    const filename = changes[changes.length - 1]?.renameUri.path;
+    return filename;
+};
+
 const isInDiffEditor = () => {
     var activeEditor = vscode.window.activeTextEditor;
     if (!activeEditor) {
@@ -72,6 +92,14 @@ const goToNextDiff = async () => {
     const lineAfter = activeEditor.selection.active.line;
 
     if (lineAfter <= lineBefore) {
+        const currentFilename = activeEditor.document.fileName;
+        const lastFilename = await getLastFilename();
+
+        if (currentFilename === lastFilename) {
+            await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+            return;
+        }
+
         await openNextFile();
     }
 
@@ -95,8 +123,15 @@ const goToPreviousDiff = async () => {
     await vscode.commands.executeCommand("workbench.action.compareEditor.previousChange");
 
     const lineAfter = activeEditor.selection.active.line;
+    const currentFilename = activeEditor.document.fileName;
+    const firstFilename = await getFirstFilename();
 
     if (lineAfter >= lineBefore) {
+        if (currentFilename === firstFilename) {
+            await vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+            return;
+        }
+
         await openPreviousFile();
     }
 
